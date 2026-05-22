8af998794980983906d7cd389adafedfad8d1e7b
arthoyos <arturoramontrinidad@gmail.com>
2026-05-22 07:18:12 +0200

    Nueva imagen casa magnolia

--- game/rooms/casa_magnolia/room_casa_magnolia.gd
+++ game/rooms/casa_magnolia/room_casa_magnolia.gd
@@ -13,7 +13,8 @@ var state: Data = load("res://game/rooms/casa_magnolia/room_casa_magnolia.tres")
 # Add any code you want to setup the stage before the room is shown to the player (e.g. setting
 # character position and facing direction, active walkable area, props visibility, etc.).
 func _on_room_entered() -> void:
-	pass
+	C.player.position = get_marker("Entrada").position
+	await C.player.walk_to(Vector2(65, 127))
 
 
 # Called after the room transition completes; the room is now visible.
